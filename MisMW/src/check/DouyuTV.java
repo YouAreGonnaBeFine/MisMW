dccccacaf2ccccaabd16ba97c073992380e22e7b
Administrator <Administrator@10.1.222.42>
2016-04-14 16:50:39 +0800

    update

--- MisMW/src/check/DouyuTV.java
+++ MisMW/src/check/DouyuTV.java
@@ -1,5 +1,6 @@
 package check;
 
+import resource.Presenter;
 import http.HttpRequest;
 import net.sf.json.JSONObject;
 
@@ -27,4 +28,30 @@ public class DouyuTV {
 		}
 	}
 
+	
+	public Presenter check(Presenter p){
+		
+		String url = "http://open.douyucdn.cn/api/RoomApi/room/"+p.getId();
+		String game = "";
+		String param = "";
+		String result = HttpRequest.sendGameStatus(url, game, param);
+		
+		JSONObject objData = JSONObject.fromObject(result);
+
+		JSONObject objRoom = objData.getJSONObject("data");
+		
+		String status = objRoom.getString("room_status");
+		String title = objRoom.getString("room_name");
+		
+		if(status!=null&&status.equals("1")){
+			
+			p.setOnLine(true);
+			p.setTitle(title);
+		}else{
+			
+			p.setOnLine(false);
+		}		
+		
+		return p;
+	}
 }
