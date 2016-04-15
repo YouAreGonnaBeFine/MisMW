144f62b107a71dc7badbec32419d28232e3fb0d0
Administrator <Administrator@10.1.222.42>
2016-04-15 11:32:09 +0800

    resource

---
+++ MisMW/src/ws/WebSocketServerHandler.java
@@ -0,0 +1,147 @@
+package ws;
+
+import static io.netty.handler.codec.http.HttpHeaders.isKeepAlive;
+import static io.netty.handler.codec.http.HttpHeaders.setContentLength;
+import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+	
+	private static final Logger logger = Logger.getLogger(WebSocketServerHandler.class.getName());
+
+	private WebSocketServerHandshaker handshaker;
+	
+	public static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		// 传统的HTTP接入
+		if (msg instanceof FullHttpRequest) {
+			handleHttpRequest(ctx, (FullHttpRequest) msg);
+		}
+		// WebSocket接入
+		else if (msg instanceof WebSocketFrame) {
+			handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+		}
+	}
+	
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		channels.add(ctx.channel());
+	}	
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
+
+	private void handleHttpRequest(ChannelHandlerContext ctx,
+			FullHttpRequest req) throws Exception {
+
+		if (!req.getDecoderResult().isSuccess()
+				|| (!"websocket".equals(req.headers().get("Upgrade")))) {
+			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1,
+					BAD_REQUEST));
+			return;
+		}
+
+		WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
+				"ws://localhost:8081/websocket", null, false);
+		handshaker = wsFactory.newHandshaker(req);
+		if (handshaker == null) {
+			WebSocketServerHandshakerFactory
+					.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+		} else {
+			handshaker.handshake(ctx.channel(), req);
+		}
+	}
+
+	private void handleWebSocketFrame(ChannelHandlerContext ctx,
+			WebSocketFrame frame) {
+
+		if (frame instanceof CloseWebSocketFrame) {
+			handshaker.close(ctx.channel(),
+					(CloseWebSocketFrame) frame.retain());
+			return;
+		}
+		if (frame instanceof PingWebSocketFrame) {
+			ctx.channel().write(
+					new PongWebSocketFrame(frame.content().retain()));
+			return;
+		}
+		if (!(frame instanceof TextWebSocketFrame)) {
+			throw new UnsupportedOperationException(String.format(
+					"%s frame types not supported", frame.getClass().getName()));
+		}
+
+		String request = ((TextWebSocketFrame) frame).text();
+		if (logger.isLoggable(Level.FINE)) {
+			logger.fine(String.format("%s received %s", ctx.channel(), request));
+		}
+		Date dat = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		String time = format.format(dat);
+		System.out.println(time+":"+request);
+		Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            if (channel != incoming){
+            	channel.writeAndFlush(new TextWebSocketFrame("$Remot:"+request));
+            } else {
+            	channel.writeAndFlush(new TextWebSocketFrame("$Local:"+request));
+            }
+        	
+        }
+		
+
+	}
+
+	private static void sendHttpResponse(ChannelHandlerContext ctx,
+			FullHttpRequest req, FullHttpResponse res) {
+		if (res.getStatus().code() != 200) {
+			ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(),
+					CharsetUtil.UTF_8);
+			res.content().writeBytes(buf);
+			buf.release();
+			setContentLength(res, res.content().readableBytes());
+		}
+
+		ChannelFuture f = ctx.channel().writeAndFlush(res);
+		if (!isKeepAlive(req) || res.getStatus().code() != 200) {
+			f.addListener(ChannelFutureListener.CLOSE);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
